9fc2bc106ebfcae6405d82c90b1a1709e0c4f857
Open Saucerer <lovedayperfection1@gmail.com>
2023-06-06 01:35:05 +0100

    feat: protect route with access key

--- src/app.ts
+++ src/app.ts
@@ -10,29 +10,38 @@ const app: Application = express();
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.raw());
-app.use(
-  cors({
-    origin(requestOrigin, callback) {
-      // allow all for now
-      return callback(null, true);
+// app.use(
+//   cors({
+//     origin(requestOrigin, callback) {
+//       // allow all for now
+//       // return callback(null, true);
+//       console.log(requestOrigin);
 
-      // switch to this when you want to restrict the origin
-      // if (requestOrigin === undefined) {
-      //     return callback(null, true);
-      // }
-      // const o = new URL(requestOrigin);
-      // // check if TLD is in the list of allowed TLDs
-      // if (env.allowedTLDs.includes(o.hostname)) {
-      //   return callback(null, true);
-      // }
-      // return callback(new Error('Not allowed by CORS'));
-    },
-    optionsSuccessStatus: 204,
-  })
-);
+//       // switch to this when you want to restrict the origin
+//       if (requestOrigin === undefined) {
+//         return callback(new Error('Not allowed by CORS'), false);
+//       }
+
+//       const o = new URL(requestOrigin!);
+
+//       // check if accessKey is in url query and if it matches the env key
+//       if (o.searchParams.get('accessKey') === env.AccessKey) {
+//         return callback(null, true);
+//       }
+
+//       return callback(new Error('Not allowed by CORS'), false);
+//     },
+//     optionsSuccessStatus: 204,
+//   })
+// );
 
 app.get('/:address/holding/:collection', async (req, res) => {
   try {
+    // validate query params
+    if (req.query.accessKey !== env.AccessKey) {
+      throw new Error('Invalid access key');
+    }
+
     const holding = await metaplex.mintlistHoldsCollection(
       req.params.collection,
       await solana.getTokenAccounts(req.params.address)
--- src/env.ts
+++ src/env.ts
@@ -1,4 +1,5 @@
 export default {
   Port: process.env.PORT || 3000,
   RPC: process.env.RPC_URL || 'https://api.devnet.solana.com',
+  AccessKey: process.env.ACCESS_KEY || 'accessKey',
 };
