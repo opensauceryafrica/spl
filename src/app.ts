c0c60b76ac58034388f554a61fc754585a6eb53e
Open Saucerer <lovedayperfection1@gmail.com>
2023-06-24 10:57:59 +0100

    fix: resolving cors error

--- src/app.ts
+++ src/app.ts
@@ -10,30 +10,7 @@ const app: Application = express();
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json({ limit: '10mb' }));
 app.use(express.raw());
-// app.use(
-//   cors({
-//     origin(requestOrigin, callback) {
-//       // allow all for now
-//       // return callback(null, true);
-//       console.log(requestOrigin);
-
-//       // switch to this when you want to restrict the origin
-//       if (requestOrigin === undefined) {
-//         return callback(new Error('Not allowed by CORS'), false);
-//       }
-
-//       const o = new URL(requestOrigin!);
-
-//       // check if accessKey is in url query and if it matches the env key
-//       if (o.searchParams.get('accessKey') === env.AccessKey) {
-//         return callback(null, true);
-//       }
-
-//       return callback(new Error('Not allowed by CORS'), false);
-//     },
-//     optionsSuccessStatus: 204,
-//   })
-// );
+app.use(cors({ origin: '*' }));
 
 app.get('/:address/holding/:collection', async (req, res) => {
   try {
